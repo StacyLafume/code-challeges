8d62bd72ea5a1eb59671b26d0fe1a88c0a985515
Stacy Lafume <stacylafume123@gmail.com>
2021-04-21 15:09:42 -0400

    Twice_as_old

---
+++ Twice_as_old.js
@@ -0,0 +1,12 @@
+// Your function takes two arguments:
+
+// current father's age (years)
+// current age of his son (years)
+// Сalculate how many years ago the father was twice as old as his son (or in how many years he will be twice as old).
+
+//https://www.codewars.com/kata/5b853229cfde412a470000d0/train/javascript
+
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+    const twice = sonYearsOld*2
+    return dadYearsOld > twice ? dadYearsOld - twice : twice - dadYearsOld
+}
